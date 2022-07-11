07277f107b329d4e03dee1e67c9de5b83b596a27
etonealbert <albert.lukmanov99@gmail.com>
2022-07-11 13:15:48 +0300

    compactMap_solved

--- problem_1.swift
+++ problem_1.swift
@@ -1,16 +1,27 @@
+// Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+// Square all numbers k (0 <= k <= n) between 0 and n.
+
+// Count the numbers of digits d used in the writing of all the k**2.
+
+// Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+
+
 func nbDig(_ n: Int, _ d: Int) -> Int {
-  
   var square_ar : Array<Int> = []
-  
-  for i in 0...n
-  {
+  var num_count = 0
+  for i in 0...n{
     square_ar.append(i*i)
   }
-  
-  
-  
-  return square_ar.count
-  // your code
+    for i in square_ar{
+    let digits = String(i).compactMap {Int(String($0))} // split number in to array of digits
+    for j in digits{
+        if j == d{
+            num_count+=1
+            }
+    }
+}
+    return num_count
 }
 
 print(nbDig(10,1))
