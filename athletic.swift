04daf05105512d466a572f0a18d4842ba8a55ccd
etonealbert <albert.lukmanov99@gmail.com>
2022-07-15 11:39:03 +0300

    wrong

--- athletic.swift
+++ athletic.swift
@@ -1,7 +1,8 @@
-func stat(_ strg: String) -> Array<Substring> {
+func stat(_ strg: String) -> String {
 
   let every_result = strg.split(separator: ",")
   var dictionary: Dictionary<Substring, Int> = [:]
+  var final_string : String = "kfk"
   //  var  : Array<String> = []
 
   for i in every_result
@@ -11,11 +12,11 @@ func stat(_ strg: String) -> Array<Substring> {
     dictionary[i] =  sum
     }
 
-    var newdictionary=dictionary.sorted { $0.1 < $1.1 }
+     let newdictionary=dictionary.sorted { $0.1 < $1.1 }
 
-    print(newdictionary)
+    final_string = "Range: \(String(newdictionary[0].0)) Average: \(String(newdictionary[(newdictionary.count/2-1)].0)) Median: \(String(newdictionary[newdictionary.count-1].0))" 
 
-  return every_result
+  return final_string
 
 }
 var l = "01|15|59,01|47|16,01|17|20,01|32|34,02|17|17";
