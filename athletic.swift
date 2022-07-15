8480fcbe5fea3ee27cd5f0beb479a3a585b92e4f
etonealbert <albert.lukmanov99@gmail.com>
2022-07-15 10:08:35 +0300

    dictionary sort

--- athletic.swift
+++ athletic.swift
@@ -1,17 +1,21 @@
-func stat(_ strg: String) -> Array<String> {
+func stat(_ strg: String) -> Array<Substring> {
 
-  var every_result : Array<String> = strg.components(separatedBy: ",")
-  var second_res : Array<Int> = []
-    var kek : Array<String> = []
+  let every_result = strg.split(separator: ",")
+  var dictionary: Dictionary<Substring, Int> = [:]
+  //  var  : Array<String> = []
 
-  for i in every_result{
-     kek+=i.split(separator: "|")
-    //second_res.append(Int(kek[0])*360+Int(kek[1])*60+Int(kek[2]))
+  for i in every_result
+  {
+    let kek = i.split(separator: "|")
+    let sum =  Int(kek[0])!*360+Int(kek[1])!*60+Int(kek[2])!
+    dictionary[i] =  sum
+    }
 
+    var newdictionary=dictionary.sorted { $0.1 < $1.1 }
 
-  }
+    print(newdictionary)
 
-  return kek
+  return every_result
 
 }
 var l = "01|15|59,01|47|16,01|17|20,01|32|34,02|17|17";
