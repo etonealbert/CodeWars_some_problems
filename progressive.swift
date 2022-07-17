d3d657dc81026212e5ff1eae6ebb121efd7158ec
etonealbert <albert.lukmanov99@gmail.com>
2022-07-17 09:04:21 +0300

    progressive

---
+++ progressive.swift
@@ -0,0 +1,18 @@
+func removNb(_ n: Int) -> [(Int,Int)]
+{
+  var prod : Array<(Int,Int)> = []
+  let sum : Int = (2+(n-1))*n/2
+  for a in 2...n-1     
+  {
+    let  b = (sum-a)/(1+a)
+    if ( (sum-a)%(1+a) == 0) && (b < n)
+    {
+      prod.append((a,b))
+    }
+  }
+  return prod
+}
+
+print(removNb(26)) 	//return to numbers wich product 
+			// is equal sum of all numbers 0 below 100
+			//except this to numbers 
