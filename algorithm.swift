cc90d3fd60ebda6260bb82aabd5f8d424b3d01b4
etonealbert <albert.lukmanov99@gmail.com>
2022-07-20 13:34:06 +0300

    tedios

--- algorithm.swift
+++ algorithm.swift
@@ -1,27 +1,39 @@
-// func ann(_ n : Int) -> [Int] {
-//     var kata_count = 1
-//         var kata_per_day : [Int] = []
-//         repeat{
-//             if(kata_per_day.count != 0){
-//                 let t = kata_count
-//                 kata_count = kata_per_day.count+1 - john(t).suffix(1)[0] 
-//             }
-//             kata_per_day.append(kata_count)
-//         }while (kata_per_day.count < n) 
-//     return kata_per_day
-// }
-// func john(_ n : Int) -> [Int] {
-//  var kata_count = 0
-//     var kata_per_day : [Int] = []
-//     repeat{
-//         if(kata_per_day.count != 0){
-//             let t = kata_count
-//             kata_count = kata_per_day.count+1 - ann(t).suffix(1)[0] 
-//         }
-//         kata_per_day.append(kata_count)
-//     }while (kata_per_day.count < n) 
-//   return kata_per_day
-// }
+func ann(_ n : Int) -> [Int] {
+    var kata_count = 1
+        var kata_per_day : [Int] = []
+        repeat{
+            if(kata_per_day.count != 0){
+                var john_t = john(kata_count)
+                kata_count = kata_per_day.count+1 - john_t.removeLast()
+            }
+            kata_per_day.append(kata_count)
+        }while ((kata_per_day.count) < n) 
+    kata_per_day.insert( 1, at: 1)
+    return kata_per_day
+}
+func john(_ n : Int) -> [Int] {
+ var kata_count = 0
+    var kata_per_day : [Int] = []
+    repeat{
+        if(kata_per_day.count != 0){
+            var ann_t = ann(kata_count)
+            kata_count = kata_per_day.count+1 - ann_t.removeLast()
+        }
+        kata_per_day.append(kata_count)
+    }while ((kata_per_day.count) < n) 
+    kata_per_day.insert( 0, at: 1)
+  return kata_per_day
+}
 
-var kek = [1, 2, 3,4 ,5 ].suffix(1)
-print("final = \(type(of:kek[0]))")
+func sumJohn(_ n : Int) -> Int {
+  
+  return john(n).reduce(0, +)
+}
+
+func sumAnn(_ n : Int) -> Int {
+  return ann(n).reduce(0, +)
+}
+var kek = john(11)
+//kek.removeLast()
+
+print("final = \(kek)")
