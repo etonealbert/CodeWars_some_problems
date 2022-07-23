b6bf4b65ab3b41a826a7a490e40b85245db8f54b
etonealbert <albert.lukmanov99@gmail.com>
2022-07-23 17:39:53 +0300

    johnann

--- algorithm.swift
+++ algorithm.swift
@@ -1,10 +1,29 @@
-func algor(_ n: Int,_ kata_kek: Int,_ id: Int) -> [Int]{
+func algor(_ n: Int,_ kata_kek: Int,_ id: Int) -> Int{
     var kata_count = kata_kek
         var kata_per_day : [Int] = []
         repeat{
             if(kata_per_day.count != 0){
                 let day_t = algor(kata_count, id, kata_kek)
-                kata_count = kata_per_day.count+1 - day_t[day_t.count-1]
+                kata_count = kata_per_day.count+1 - day_t
+            }
+            kata_per_day.append(kata_count)
+        }while ((kata_per_day.count) < n) 
+    return Int(kata_per_day[kata_per_day.count - 1])
+}
+
+
+func algor2(_ n: Int,_ kata_kek: Int,_ id: Int) -> [Int]{
+    var kata_count = kata_kek
+        var kata_per_day : [Int] = []
+        repeat{
+            if(kata_per_day.count != 0){
+               
+                if(kata_per_day.count > n){
+                     var day_t = algor2(kata_count, id, kata_kek)
+                     kata_count = kata_per_day.count+1 - day_t.removeLast()
+                }else{
+                 let day_t = algor(kata_count, id, kata_kek)
+                kata_count = kata_per_day.count+1 - day_t}
             }
             kata_per_day.append(kata_count)
         }while ((kata_per_day.count) < n) 
@@ -12,14 +31,14 @@ func algor(_ n: Int,_ kata_kek: Int,_ id: Int) -> [Int]{
 }
 
 func ann(_ n : Int) -> [Int] {
-    var answer = algor(n, 1, 0)
+    var answer = algor2(n, 1, 0)
      answer.insert( 1, at: 1)
     answer.removeLast()
     return answer
 }
 
 func john(_ n : Int) -> [Int] {
- var answer = algor(n, 0, 1)
+ var answer = algor2(n, 0, 1)
  answer.insert( 0, at: 1)
  answer.removeLast()
     return answer
---
+++ grasshopper.java
@@ -0,0 +1,11 @@
+class grasshopper {
+    
+    static String greet(String name, String owner) {
+      
+      
+        // Add code here
+    }
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
